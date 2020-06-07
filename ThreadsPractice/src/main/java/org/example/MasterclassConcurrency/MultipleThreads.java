bf6761d6c6e7c55ee559a87bbc5ea67eb3503c2f
alpizano <apizano@purdue.edu>
2020-06-07 02:05:47 -0500

    Using a different countdown object

--- ThreadsPractice/src/main/java/org/example/MasterclassConcurrency/MultipleThreads.java
+++ ThreadsPractice/src/main/java/org/example/MasterclassConcurrency/MultipleThreads.java
@@ -3,9 +3,10 @@ package org.example.MasterclassConcurrency;
 public class MultipleThreads {
     public static void main(String[] args) {
         Countdown countdown = new Countdown();
+        Countdown countdown2 = new Countdown();
         CountdownThread t1 = new CountdownThread(countdown);
         t1.setName("Thread 1");
-        CountdownThread t2 = new CountdownThread(countdown);
+        CountdownThread t2 = new CountdownThread(countdown2);
         t2.setName("Thread 2");
 
         t1.start();
