7b8a4a06eff788840fab9d40e5e60ef9f8856e1a
alpizano <apizano@purdue.edu>
2020-06-14 19:23:53 -0500

    Handling exceptions with FileWriter

---
+++ ExceptionsIO/src/main/java/org/example/inputoutput/FileWriterTest.java
@@ -0,0 +1,36 @@
+package org.example.inputoutput;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class FileWriterTest {
+    public static void main(String[] args)  {
+        FileWriter locFile = null;
+        try {
+            locFile = new FileWriter("monkeybone.txt");
+            locFile.write("Go to park \n Go to house \n");
+
+        }
+        catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+        finally {
+            System.out.println("In finally block");
+            try {
+                if(locFile != null) {
+                    // Have to handle IOException causes by attempting to clos stream
+                    locFile.close();
+                }
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+
+
+
+    }
+
+}
