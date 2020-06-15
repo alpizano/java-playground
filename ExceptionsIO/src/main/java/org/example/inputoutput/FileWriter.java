2af031437f28aaa842ecf1c63571889ac53736db
alpizano <apizano@purdue.edu>
2020-06-14 19:16:00 -0500

    Adding Locations class

--- ExceptionsIO/src/main/java/org/example/inputoutput/FileWriter.java
+++ ExceptionsIO/src/main/java/org/example/inputoutput/FileWriter.java
@@ -4,9 +4,9 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 
-public class FileWrite {
+public class FileWriter {
     public static void main(String[] args) throws IOException {
-        FileWriter locFile = new FileWriter("monkeybone.txt");
+        java.io.FileWriter locFile = new java.io.FileWriter("monkeybone.txt");
         locFile.write("Go to park \n On Fridays \n Sun is great \n Another Sentence");
 
         // Remember to close stream
