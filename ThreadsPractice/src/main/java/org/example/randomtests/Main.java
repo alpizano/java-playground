1b42c2c5ada239c165549c960011998ade554fbe
alpizano <apizano@purdue.edu>
2020-06-21 02:19:46 -0500

    Corrected by adding blocking (Scanner) logic to new Thread, and File IO in main thread, concurrency achieved

--- ThreadsPractice/src/main/java/org/example/randomtests/Main.java
+++ ThreadsPractice/src/main/java/org/example/randomtests/Main.java
@@ -8,44 +8,43 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        // Read user input
-        Scanner scanner = new Scanner(System.in);
+
 
         // Read a file
         Thread t1 = new Thread(new Runnable() {
             @Override
             public void run() {
-                // Use Try with Resource, catch the IOException
-                try(BufferedReader in = new BufferedReader(new FileReader("test.txt"))) {
-                    String line;
+                Scanner scanner = new Scanner(System.in);
 
-                    while((line = in.readLine()) != null) {
-                        System.out.println(line);
-                    }
-                }
-                catch(IOException e) {
-                    System.out.println(e.toString());
+                // Scanner is blocking; Cannot read file while is running, need to spawn new Thread
+                while(scanner.hasNext()) {
+                    System.out.println(scanner.nextLine());
                 }
 
             }
         });
 
-        // Scanner is blocking; Cannot read file while is running, need to spawn new Thread
-        while(scanner.hasNext()) {
-            System.out.println(scanner.nextLine());
-        }
+
+        t1.start();
+
+
+
 
         //int foo = scanner.nextInt();
 
 
+        // Use Try with Resource, catch the IOException
+        try(BufferedReader in = new BufferedReader(new FileReader("test.txt"))) {
+            String line;
 
-        t1.start();
+            while((line = in.readLine()) != null) {
+                System.out.println(line);
+            }
+        }
+        catch(IOException e) {
+            System.out.println(e.toString());
+        }
 
-//        BufferedReader in = new BufferedReader(new FileReader("test.txt"));
-//        String line;
-//
-//        while((line = in.readLine()) != null) {
-//            System.out.println(line);
-//        }
+
     }
 }
