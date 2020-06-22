a54ef4200c4942340bbe77713d646d1ef8c05039
alpizano <apizano@purdue.edu>
2020-06-22 00:59:05 -0500

    Adding Writer classO

--- ThreadsPractice/src/main/java/org/example/MasterclassConcurrency/prodconsum/Main.java
+++ ThreadsPractice/src/main/java/org/example/MasterclassConcurrency/prodconsum/Main.java
@@ -1,5 +1,7 @@
 package org.example.MasterclassConcurrency.prodconsum;
 
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
 
@@ -19,11 +21,40 @@ class Message {
         return message;
     }
 
+    // Used by Producer
     public synchronized void write(String message) {
         while(!empty) {
 
         }
         empty = false;
         this.message = message;
+    }
+}
+
+class Writer implements Runnable {
+    private Message message;
+    public Writer(Message message) {
+        this.message = message;
+    }
+
+    public void run() {
+        String[] messages = new String[]{"Humpty Dumpty sat on a wall ",
+                "Humpty Dumpty had a great fall",
+                "All the king's horses and all the king's men",
+                "Couldn't put Humpty together again"};
+
+        Random random = new Random();
+
+        for(int i=0; i<messages.length; i++) {
+            message.write(messages[i]);
+            try {
+                // Sleep for 2 secs after writing message
+                Thread.sleep(random.nextInt(2000));
+            }
+            catch (InterruptedException e) {
+
+            }
+            message.write("Finished");
+        }
     }
 }
