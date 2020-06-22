665ba01fe29fd1f0f4ee6fd87301d4a50e79f1ad
alpizano <apizano@purdue.edu>
2020-06-21 21:14:33 -0500

    Added Deserialization of Car object via ObjectInputStream, BufferedInputStream, and FileInputStream

--- FileIO/src/main/java/org/example/serialization/Main.java
+++ FileIO/src/main/java/org/example/serialization/Main.java
@@ -1,21 +1,39 @@
 package org.example.serialization;
 
-import java.io.BufferedOutputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.ObjectOutputStream;
+import java.io.*;
+import java.nio.Buffer;
 
 public class Main {
     public static void main(String[] args) {
         Car car = new Car("toyota", "camry", 2004);
 
         // Try w/ resources
-        // ObjectOutputStream is for serializating Java objects
-        try(ObjectOutputStream out = new ObjectOutputStream(new BufferedOutputStream(new FileOutputStream("car.dat")))) {
+        // ObjectOutputStream is for serializing Java objects
+        try (ObjectOutputStream out = new ObjectOutputStream(new BufferedOutputStream(new FileOutputStream("car.dat")))) {
             out.writeObject(car);
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+        try (ObjectInputStream in = new ObjectInputStream(new BufferedInputStream(new FileInputStream("car.dat")))) {
+            boolean eof = false;
+            while (!eof) {
+                try {
+                    // Need to cast Object to Car (Narrowing)
+                    Car car2 = (Car) in.readObject();
+                    System.out.println(car2.make + " " +  car2.model + " " + car2.year);
+                }
+                catch(EOFException e) {
+                    eof = true;
+                    System.out.println(e.getMessage());
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
 
     }
 }
